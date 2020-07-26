7c4fa3c619c12c7979379b2136abe887a6ec842f
nnian <48366474+nnian@users.noreply.github.com>
2020-07-26 22:20:54 +0200

    Initial Commit

---
+++ src/config.ts
@@ -0,0 +1,44 @@
+let global: any = typeof window !== 'undefined' ? window : self;
+global.config = {
+	apiUrl: [
+		"https://01-cache.testnet.myqwertycoin.com/"
+	],
+	nodeList: [
+		"https://testnet.myqwertycoin.com/nodesvr/",
+		"https://private.myqwertycoin.com/api/?mode=get&url=http://node-00.testnet.qwertycoin.org:8197/",
+		"https://private.myqwertycoin.com/api/?mode=get&url=http://node-01.testnet.qwertycoin.org:8197/"
+	],
+	electionApiUrl: "https://voting.qwertycoin.org/api",
+	websiteApiUrl: "https://www.qwertycoin.org/wp-json",
+	mainnetexplorer.testnetUrl: "https://explorer.testnet.qwertycoin.org/",
+	mainnetexplorer.testnetUrlHash: "https://explorer.testnet.qwertycoin.org/?hash={ID}#blockchain_transaction",
+	mainnetexplorer.testnetUrlBlock: "https://explorer.testnet.qwertycoin.org/?hash={ID}#blockchain_block",
+	testnetexplorer.testnetUrl: "https://explorer.testnet.qwertycoin.org/",
+	testnetexplorer.testnetUrlHash: "https://explorer.testnet.qwertycoin.org/?hash={ID}#blockchain_transaction",
+	testnetexplorer.testnetUrlBlock: "https://explorer.testnet.qwertycoin.org/?hash={ID}#blockchain_block",
+	testnet: false,
+	coinUnitPlaces: 8,
+	coinDisplayUnitPlaces: 2,
+	txMinConfirms: 10,
+	txCoinbaseMinConfirms: 10,
+	addressPrefix: 0x14820c,
+	integratedAddressPrefix: 0x148201,
+	addressPrefixTestnet: 0x14820c,
+	integratedAddressPrefixTestnet: 0x148201,
+	subAddressPrefix: 0x148202,
+	subAddressPrefixTestnet: 0x148202,
+	coinFee: new JSBigInt('100000000'),
+	feePerKB: new JSBigInt('100000000'),
+	dustThreshold: new JSBigInt('100000'), //used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
+	defaultMixin: 0, // default value mixin
+
+	idleTimeout: 30,
+	idleWarningDuration: 20,
+
+	coinSymbol: 'QWC',
+	openAliasPrefix: "qwc",
+	coinName: 'Qwertycoin',
+	coinUriPrefix: 'qwertycoin:',
+	avgBlockTime: 120,
+	maxBlockNumber: 500000000,
+};
---
+++ src/translations/README.md
@@ -0,0 +1,64 @@
+![image](https://cdn.qwertycoin.org/images/press/other/qwc-github-3.png)
+
+# Translation files used for web- and mobile wallet!
+
+
+## How To Translate
+Download our translation files:
+```
+git clone https://github.com/qwertycoin-org/translation-mobileweb
+```
+
+[Download][zip] our latest Version of the QWC Translation Editor
+
+Open the cloned folder with the translationfiles aaand lets go: 
+
+![screenshot-mainwindow](https://github.com/qwertycoin-org/JsonTranslationEditor/blob/master/QWCTranslationEditor/Assets/Images/screenshot1.png "Main Window")
+
+## And then?
+
+Coming soon ...
+
+
+### Translations:
+- English: too many people
+- French: gnock
+- Serbian cyrillic: girugameshh
+- German: F0sching, Alex Nnian
+- Hungarian: Gelesztaa
+- Greek: GeraltOfTrivia
+- Russian, Ukrainian: Aiwe
+- Persian: M4hdi1995
+- Japanse: ```need translator```
+- Korean: Xecute0101
+- Chinese: ```need translator```
+- Arabic: ```need translator```
+- Hindi: ```need translator```
+- Turkish: ```need translator```
+
+## Donate
+
+```
+QWC: QWC1K6XEhCC1WsZzT9RRVpc1MLXXdHVKt2BUGSrsmkkXAvqh52sVnNc1pYmoF2TEXsAvZnyPaZu8MW3S8EWHNfAh7X2xa63P7Y
+```
+```
+BTC: 1DkocMNiqFkbjhCmG4sg9zYQbi4YuguFWw
+```
+```
+ETH: 0xA660Fb28C06542258bd740973c17F2632dff2517
+```
+```
+BCH: qz975ndvcechzywtz59xpkt2hhdzkzt3vvt8762yk9
+```
+```
+XMR: 47gmN4GMQ17Veur5YEpru7eCQc5A65DaWUThZa9z9bP6jNMYXPKAyjDcAW4RzNYbRChEwnKu1H3qt9FPW9CnpwZgNscKawX
+```
+```
+ETN: etnkJXJFqiH9FCt6Gq2HWHPeY92YFsmvKX7qaysvnV11M796Xmovo2nSu6EUCMnniqRqAhKX9AQp31GbG3M2DiVM3qRDSQ5Vwq
+```
+
+#### Thanks
+
+Qwertycoin Community
+
+[zip]: https://github.com/qwertycoin-org/JsonTranslationEditor/releases/download/v1.0.1/Qwertycoin-Translation-Editor_v1.0.1.7.zip
